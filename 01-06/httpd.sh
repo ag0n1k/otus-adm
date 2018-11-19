61b1bbd579b2af4ffc4d6a4ddba6cc4508d42a1c
a.vazhenin <a.vazhenin@cleverdata.ru>
2018-11-19 21:25:14 +0300

    01-06: httpd many instances fixed

--- 01-06/httpd.sh
+++ 01-06/httpd.sh
@@ -8,10 +8,26 @@ sudo cp /vagrant/httpd/httpd@.service /usr/lib/systemd/system
 sudo cp /vagrant/httpd/httpd-1000 /etc/sysconfig/
 sudo cp /vagrant/httpd/httpd-1001 /etc/sysconfig/
 
+sudo mkdir /var/log/httpd2
+sudo mkdir /opt/httpd2
+sudo mkdir /run/httpd2
+sudo cp -r /etc/httpd/* /opt/httpd2
+
+sudo rm /opt/httpd2/logs
+sudo rm /opt/httpd2/modules
+sudo rm /opt/httpd2/run
+sudo ln -s /var/log/httpd2 /opt/httpd2/logs
+sudo ln -s /usr/lib64/httpd/modules /opt/httpd2/modules
+sudo ln -s /run/httpd2 /opt/httpd2/run
+
+sudo cp /vagrant/httpd/httpd.conf /opt/httpd2/conf/
+
+sudo restorecon /
+
 sudo systemctl daemon-reload
 
-echo "#note: the second server will not start because of config file the same, but:"
-echo "service can handle many configurations via @ =)"
-echo "next line is sudo systemctl start httpd@1001"
-
 sudo systemctl start httpd@1001
+sudo systemctl start httpd@1000
+
+sudo systemctl status httpd@1001
+sudo systemctl status httpd@1000
